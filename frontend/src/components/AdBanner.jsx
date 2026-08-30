ff76c2e8c4493abb71da752b7132eab5672ccd80
RAKIBUL424 <rakibul.r1991@gmail.com>
2026-08-30 10:21:25 +0600

    between two page ads test

--- frontend/src/components/AdBanner.jsx
+++ frontend/src/components/AdBanner.jsx
@@ -1,7 +1,56 @@
-import { useEffect } from "react";
+// import { useEffect } from "react";
+
+// export default function AdBanner() {
+//   useEffect(() => {
+//     const script = document.createElement("script");
+
+//     script.async = true;
+//     script.setAttribute("data-cfasync", "false");
+//     script.src =
+//       "https://pl31089864.profitableratecpmnetwork.com/ee013dcd506c4682e79beb89ec5789d0/invoke.js";
+
+//     const container = document.getElementById(
+//       "container-ee013dcd506c4682e79beb89ec5789d0"
+//     );
+
+//     if (container) {
+//       container.innerHTML = "";
+//       container.appendChild(script);
+//     }
+
+//     return () => {
+//       if (container) {
+//         container.innerHTML = "";
+//       }
+//     };
+//   }, []);
+
+//   return (
+//     <div
+//       style={{
+//         width: "100%",
+//         margin: "25px 0",
+//         minHeight: "100px",
+//         overflow: "hidden",
+//       }}
+//     >
+//       <div id="container-ee013dcd506c4682e79beb89ec5789d0"></div>
+//     </div>
+//   );
+// }
+
+
+import { useEffect, useRef } from "react";
 
 export default function AdBanner() {
+  const adContainerRef = useRef(null);
+
   useEffect(() => {
+    if (!adContainerRef.current) return;
+
+    // পরিষ্কার করি
+    adContainerRef.current.innerHTML = "";
+
     const script = document.createElement("script");
 
     script.async = true;
@@ -9,18 +58,11 @@ export default function AdBanner() {
     script.src =
       "https://pl31089864.profitableratecpmnetwork.com/ee013dcd506c4682e79beb89ec5789d0/invoke.js";
 
-    const container = document.getElementById(
-      "container-ee013dcd506c4682e79beb89ec5789d0"
-    );
-
-    if (container) {
-      container.innerHTML = "";
-      container.appendChild(script);
-    }
+    adContainerRef.current.appendChild(script);
 
     return () => {
-      if (container) {
-        container.innerHTML = "";
+      if (adContainerRef.current) {
+        adContainerRef.current.innerHTML = "";
       }
     };
   }, []);
@@ -34,7 +76,10 @@ export default function AdBanner() {
         overflow: "hidden",
       }}
     >
-      <div id="container-ee013dcd506c4682e79beb89ec5789d0"></div>
+      <div
+        id="container-ee013dcd506c4682e79beb89ec5789d0"
+        ref={adContainerRef}
+      />
     </div>
   );
 }
